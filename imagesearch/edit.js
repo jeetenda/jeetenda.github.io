c2f079703182b889d8bbcf307e87b7c20ba2723d
jeetenda <45250795+jeetenda@users.noreply.github.com>
2019-12-26 13:25:26 +0530

    Update edit.js

--- imagesearch/edit.js
+++ imagesearch/edit.js
@@ -1,15 +1,15 @@
-var param1var = getQueryVariable("img"
-function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if (pair[0] == variable) {
-      return pair[1];
-    }
-  } 
+// var param1var = getQueryVariable("img"
+// function getQueryVariable(variable) {
+//   var query = window.location.search.substring(1);
+//   var vars = query.split("&");
+//   for (var i=0;i<vars.length;i++) {
+//     var pair = vars[i].split("=");
+//     if (pair[0] == variable) {
+//       return pair[1];
+//     }
+//   } 
   
-}
+// }
 
 var str = window.location.href;
 var res = str.replace("https://jeetenda.github.io/imagesearch/edit_image.html?img=", "");
